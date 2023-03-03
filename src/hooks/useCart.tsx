fa21d0d63b68ce3595f0eac2bd145bea2d7e8ef8
Robetjunior <juniorferreirarob@gmail.com>
2023-03-03 18:02:18 -0300

    update Header

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -23,20 +23,28 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
+  
+
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const product = cart.filter(product => product.id === productId)
+
+      if(product){
+        //product.amount += 1
+
+        return 
+      }
     } catch {
-      // TODO
+      const notify = () => toast("Não foi possível adicionar esse item no carrinho!")
     }
   };
 
